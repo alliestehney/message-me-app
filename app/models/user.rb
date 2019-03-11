057971dda48a60459ad26d81a7e2dd9e91ff26e3
alliestehney <allie@quikly.com>
2019-03-11 19:04:36 -0400

    Flash message stylings; cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: true
-  has_secure_password
+  validates :username, presence: true, length: { minimum: 5, maximum: 15 }, uniqueness: { case_sensitive: false }
 
   has_many :messages
+  has_secure_password
 end
